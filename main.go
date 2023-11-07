57bcce8ff00115a1cbd679bb2d479ad8ffe4a95b
Greg Mohler <110078080+CallMeGreg@users.noreply.github.com>
2023-11-06 17:42:14 -0800

    code cleanup, added language filter

--- main.go
+++ main.go
@@ -11,6 +11,23 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type languageDetails struct {
+	Size int `json:"size"`
+	Node struct {
+		Name string `json:"name"`
+	} `json:"node"`
+}
+
+type Repo struct {
+	Languages     []languageDetails `json:"languages"`
+	NameWithOwner string            `json:"nameWithOwner"`
+}
+
+type languageCount struct {
+	name  string
+	count int
+}
+
 func _main() error {
 	rootCmd := &cobra.Command{
 		Use:   "language <subcommand> [flags]",
@@ -18,7 +35,8 @@ func _main() error {
 	}
 
 	rootCmd.PersistentFlags().IntP("limit", "l", 100, "The maximum number of repositories to evaluate")
-	rootCmd.PersistentFlags().IntP("top", "t", 10, "Return the top N languages")
+	rootCmd.PersistentFlags().IntP("top", "t", 10, "Return the top N languages (ignored when a language is specified)")
+	rootCmd.PersistentFlags().StringP("language", "L", "", "The language to filter on")
 
 	rootCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) (err error) {
 		return
@@ -50,14 +68,26 @@ func runCount(cmd *cobra.Command, args []string) (err error) {
 	}
 	fmt.Printf("Analyzing organization: %s\n", org)
 
+	languageFilter, _ := cmd.Flags().GetString("language")
+	top, _ := cmd.Flags().GetInt("top")
+
 	repoLimit, _ := cmd.Flags().GetInt("limit")
 	if repoLimit > 0 {
 		fmt.Printf("Limiting to %d repositories.\n", repoLimit)
+	} else {
+		fmt.Printf("Please select a repository limit greater than 0.\n")
+		return
 	}
 
-	top, _ := cmd.Flags().GetInt("top")
-	if top > 0 {
-		fmt.Printf("Returning the top %d languages.\n", top)
+	if languageFilter != "" {
+		fmt.Printf("Filtering on language: %s\n", languageFilter)
+	} else {
+		if top > 0 {
+			fmt.Printf("Returning the top %d languages.\n", top)
+		} else {
+			fmt.Printf("Please select a top languages value greater than 0.\n")
+			return
+		}
 	}
 
 	repo_cmd := exec.Command("gh", "repo", "list", org, "--limit", strconv.FormatInt(int64(repoLimit), 10), "--json", "nameWithOwner,languages")
@@ -65,17 +95,6 @@ func runCount(cmd *cobra.Command, args []string) (err error) {
 	if err != nil {
 		fmt.Printf("Error: %s\n", err.Error())
 		return
-	}
-
-	type jsonLanguage struct {
-		Size int `json:"size"`
-		Node struct {
-			Name string `json:"name"`
-		} `json:"node"`
-	}
-
-	type Repo struct {
-		Languages []jsonLanguage `json:"languages"`
 	}
 
 	var repos []Repo
@@ -86,24 +105,19 @@ func runCount(cmd *cobra.Command, args []string) (err error) {
 		return
 	}
 
-	languageCount := make(map[string]int)
-	repoCount := len(repos)
+	languageMap := make(map[string]int)
+	analyzedRepoCount := len(repos)
 
 	for _, repo := range repos {
 		for _, language := range repo.Languages {
-			languageCount[language.Node.Name]++
+			languageMap[language.Node.Name]++
 		}
 	}
 
-	type outputLanguage struct {
-		name  string
-		count int
-	}
-
 	// convert the map to a slice
-	languages := make([]outputLanguage, 0, len(languageCount))
-	for name, count := range languageCount {
-		languages = append(languages, outputLanguage{name, count})
+	languages := make([]languageCount, 0, len(languageMap))
+	for name, count := range languageMap {
+		languages = append(languages, languageCount{name, count})
 	}
 
 	// sort the slice by count in descending order
@@ -111,14 +125,21 @@ func runCount(cmd *cobra.Command, args []string) (err error) {
 		return languages[i].count > languages[j].count
 	})
 
-	// print out the top N languages, along with the percent frequency
-	counter := 0
-	for _, lang := range languages {
-		if counter >= top {
-			break
+	// if a language was specified, only print that result:
+	if languageFilter != "" {
+		// get the language details from the map:
+		languageCount := languageMap[languageFilter]
+		fmt.Printf("%*d repos (%*d%%) that include the language: %s\n", len(strconv.Itoa(analyzedRepoCount)), languageCount, 2, (languageCount*100)/analyzedRepoCount, languageFilter)
+	} else {
+		// print out the top N languages, along with the percent frequency
+		counter := 0
+		for _, lang := range languages {
+			if counter >= top {
+				break
+			}
+			fmt.Printf("%*d repos (%*d%%) that include the language: %s\n", len(strconv.Itoa(analyzedRepoCount)), lang.count, 2, (lang.count*100)/analyzedRepoCount, lang.name)
+			counter++
 		}
-		fmt.Printf("%*d repos (%d%%) that include the language: %s\n", len(strconv.Itoa(repoCount)), lang.count, (lang.count*100)/repoCount, lang.name)
-		counter++
 	}
 
 	return
