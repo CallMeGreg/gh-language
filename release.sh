e2ef3cce4a14b251bd2bd83e9a2145a376ca891a
Greg Mohler <callmegreg@github.com>
2025-05-28 22:34:11 -0400

    action pinning, release command, gitignore cleanup

---
+++ release.sh
@@ -0,0 +1,7 @@
+export version=v2.0.0
+git tag $version
+git push origin $version
+GOOS=windows GOARCH=amd64 go build -o gh-language-windows-amd64.exe
+GOOS=linux GOARCH=amd64 go build -o gh-language-linux-amd64
+GOOS=darwin GOARCH=amd64 go build -o gh-language-darwin-amd64
+gh release create $version ./*amd64*
